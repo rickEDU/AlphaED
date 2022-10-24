613382055292220d1b7a0a39ae02987258f28d19
rickEDU <eduardo.h3nriqu3@gmail.com>
2022-10-24 19:38:14 -0300

    atualização da questão 2

--- ex2/index.js
+++ ex2/index.js
@@ -1,12 +1,25 @@
+function comparaNum(){
+    let num1= document.querySelector("#num1").value
+    let num2= document.querySelector("#num2").value
+    if(isNaN(num1)|| isNaN(num2)){
+        document.querySelector("#saida").textContent = "ERRO!! Insira somente números"
+    }else if (num1>num2){
+        document.querySelector("#saida").textContent = `valor 1(${num1}) é maior que o valor 2(${num2})`
+    }else if (num1<num2){
+        document.querySelector("#saida").textContent = `valor 1(${num1}) é menor que o valor 2(${num2})`
+    }else if (num1===num2){
+        document.querySelector("#saida").textContent = `valor 1(${num1}) é igual que o valor 2(${num2})`
+    }
+}
 
-function imprime (){
+function comparaString (){
     entrada1= document.getElementById("entrada1").value
     entrada2= document.getElementById("entrada2").value
     if (entrada1.length>entrada2.length){
-        document.getElementById("saida").innerHTML= "A primeira palavra é maior que a segunda.";
+        document.getElementById("saida2").innerHTML= "A primeira palavra é maior que a segunda.";
     }else if (entrada1.length<entrada2.length){
-        document.getElementById("saida").innerHTML= "A segunda palavra é maior que a primeira.";
+        document.getElementById("saida2").innerHTML= "A segunda palavra é maior que a primeira.";
     }else{
-        document.getElementById("saida").innerHTML= "As duas palavras tem o mesmo tamanho.";
+        document.getElementById("saida2").innerHTML= "As duas palavras tem o mesmo tamanho.";
     }
 }
