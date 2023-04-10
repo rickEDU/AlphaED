3c6ddf04210b75888bb513eab9612c4d73c6211a
Eduardo Henrique Silva Santos <eduardo.h3nriqu3@gmail.com>
2023-04-10 08:14:54 -0300

    retirando os dist

--- Typescript/ex6/q1/dist/serve.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
-const dotenv_1 = require("dotenv");
-(0, dotenv_1.config)();
-new app_1.App().server.listen(process.env.PORT, () => {
-    console.log(`Servidor rodando na em http://${process.env.HOST}:${process.env.PORT}`);
-});
