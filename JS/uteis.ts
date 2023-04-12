98a7d7dd64b14e8407fae6923ae31faeb0a5ab90
Eduardo Henrique Silva Santos <eduardo.h3nriqu3@gmail.com>
2023-04-11 22:29:24 -0300

    tabela octal regex

--- JS/uteis.ts
+++ JS/uteis.ts
@@ -1,8 +1,15 @@
+// exemplo de input na função para calcular a idade:
 const dataNascimento = new Date(1993,8,23)
-
+// função para calcular a iddade:
 function calcularIdade(dataDeNascimento: Date):number {
   const dataAtual = new Date();
   const diferencaEmMilissegundos = dataAtual.getTime() - dataDeNascimento.getTime();
   const idadeEmAnos = Math.floor(diferencaEmMilissegundos / (1000 * 60 * 60 * 24 * 365.25));
   return idadeEmAnos;
 }
+
+//Regex seboso:
+// usando a tabela de octal para excluir os caracteres especiais 
+//link: https://docs.generic-mapping-tools.org/6.2/_images/GMT_App_F_stand+_iso+.png
+const name ="teste"
+name = name.replace(/[\031-\037]/g, "")
